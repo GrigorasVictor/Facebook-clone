5acdc03e85fe0f3f4581b295bddc4ec19acc74fd
GrigorasVictor <victorandrei201112@gmail.com>
2025-03-06 21:06:18 +0200

    added TODOS:

--- server/src/main/java/facebook/server/controller/UserController.java
+++ server/src/main/java/facebook/server/controller/UserController.java
@@ -8,4 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController extends AbstractController<User, UserRepository> {
+    //TODO: implement login
+    //TODO: implement signup
+    //TODO: implement logout
+    //TODO: implement routing with JWT (adica sa nu poti accesa anumite rute fara sa fii logat)
 }
