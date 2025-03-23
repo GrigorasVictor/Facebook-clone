181f38ca6fa7689ce67b2ed015c2ae374860ae57
GrigorasVictor <victorandrei201112@gmail.com>
2025-03-24 01:06:14 +0200

    added service crud operations

--- auth/src/main/java/facebook/auth/controller/UserController.java
+++ auth/src/main/java/facebook/auth/controller/UserController.java
@@ -3,5 +3,7 @@ package facebook.auth.controller;
 import facebook.auth.entity.UserAuthentification;
 import facebook.auth.service.UserAuthentificationService;
 
+
+//idk if we need this controller
 public class UserController extends AbstractController<UserAuthentification, UserAuthentificationService> {
 }
--- server/src/main/java/facebook/server/controller/AbstractController.java
+++ server/src/main/java/facebook/server/controller/AbstractController.java
@@ -16,34 +16,31 @@ public abstract class AbstractController<T, S extends AbstractService<T, ? exten
 
     @PostMapping
     public ResponseEntity<T> add(@RequestBody T newEntry) {
-        System.out.println(newEntry);
-        service.getRepository().save(newEntry);
+        service.save(newEntry);
         return new ResponseEntity<>(newEntry, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<T> delete(@PathVariable Long id) {
-        service.getRepository().deleteById(id);
+    public ResponseEntity<T> delete(@PathVariable Long id){
+        service.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<List<T>> getAll() {
-        return new ResponseEntity<>(service.getRepository().findAll(), HttpStatus.OK);
+    public ResponseEntity<List<T>> getAll(){
+        return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<T>> get(@PathVariable Long id) {
-        return new ResponseEntity<>(service.getRepository().findById(id), HttpStatus.OK);
+    public ResponseEntity<Optional<T>> get(@PathVariable Long id){
+        return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<T> update(@PathVariable Long id, @RequestBody T updatedEntry) {
-        if (service.getRepository().existsById(id)) {
-            service.getRepository().save(updatedEntry);
-            return new ResponseEntity<>(updatedEntry, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        T newEntity = service.update(id, updatedEntry);
+
+        return newEntity == null ? new ResponseEntity<>(updatedEntry, HttpStatus.OK) :
+                new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
